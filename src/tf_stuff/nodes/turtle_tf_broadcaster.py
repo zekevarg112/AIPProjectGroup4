22a07cc7bdd327140faa38bff408db09df83a0ab
zekevarg112 <kasper.georen@gmail.com>
2024-02-14 11:12:20 +0100

    Added apriltag and cleaned up old test files

---
+++ src/tf_stuff/nodes/turtle_tf_broadcaster.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python  
+import roslib
+roslib.load_manifest('tf_stuff')
+
+import rospy
+import tf
+
+if __name__ == '__main__':
+    rospy.init_node('fixed_tf_broadcaster')
+    br = tf.TransformBroadcaster()
+    rate = rospy.Rate(0.5)
+    while not rospy.is_shutdown():
+        br.sendTransform((0.0, 0.0, 0.0),
+                         (0.0, 0.0, 0.0, 1.0),
+                         rospy.Time.now(),
+                         "camera",
+                         "camera_link")
+        rate.sleep()
