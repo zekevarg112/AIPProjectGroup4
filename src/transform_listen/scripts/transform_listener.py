ea8bcd67d862fe7750ff219acb44fb0476d270a4
Linus <linus.sjostrand@live.se>
2024-02-22 16:09:01 +0100

    yea

--- src/transform_listen/scripts/transform_listener.py
+++ src/transform_listen/scripts/transform_listener.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import yaml
 import tf
 import rospy
 
@@ -11,8 +11,8 @@ listener = tf.TransformListener()
 print("Node initialized, looking for tag positions")
 while not rospy.is_shutdown():
 	try:
-		(trans,rot) = listener.lookupTransform('/map', '/tag_4', rospy.Time(0))
-		print(trans)
+		(trans,rot) = listener.lookupTransform('/map', '/tag_5', rospy.Time(0))
+		output = yaml.dump(trans, sort_keys=False)
 		rospy.Rate(2).sleep()
                         
 	except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
