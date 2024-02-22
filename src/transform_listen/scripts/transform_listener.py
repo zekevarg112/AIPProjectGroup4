03ddee3e7ff5980ee2eb1db7f349c59de641a5af
Linus <linus.sjostrand@live.se>
2024-02-22 16:36:10 +0100

    python new

--- src/transform_listen/scripts/transform_listener.py
+++ src/transform_listen/scripts/transform_listener.py
@@ -4,25 +4,37 @@ import tf
 import rospy
 
 
+def write_yaml_to_file(py_obj):
+    with open('output.yaml', 'w',) as f :
+        yaml.dump(py_obj,f,sort_keys=False) 
+    print('Written to file successfully')
+
+
+
+
 rospy.init_node("tf_listener")
 
 listener = tf.TransformListener()
+
+    
 
 print("Node initialized, looking for tag positions")
 while not rospy.is_shutdown():
 	try:
 		(trans,rot) = listener.lookupTransform('/map', '/tag_5', rospy.Time(0))
-		write_yaml_to_file(trans)
+		trans_dict={
+			"X":trans[0],
+			"Y":trans[1],
+			"Z":trans[2]
+		}
+			
+		write_yaml_to_file(trans_dict)
 		rospy.Rate(2).sleep()
                         
 	except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 		#continuerospa
 		continue
 		
-def write_yaml_to_file(py_obj):
-    with open('output.yaml', 'w',) as f :
-        yaml.dump(py_obj,f,sort_keys=False) 
-    print('Written to file successfully')
-    
+
     
 
