b542e05bf94ea37298cc82cafa787c052298c859
Linus <linus.sjostrand@live.se>
2024-02-22 16:12:02 +0100

    yaml capacitet

--- src/transform_listen/scripts/transform_listener.py
+++ src/transform_listen/scripts/transform_listener.py
@@ -12,9 +12,17 @@ print("Node initialized, looking for tag positions")
 while not rospy.is_shutdown():
 	try:
 		(trans,rot) = listener.lookupTransform('/map', '/tag_5', rospy.Time(0))
-		output = yaml.dump(trans, sort_keys=False)
+		write_yaml_to_file(trans)
 		rospy.Rate(2).sleep()
                         
 	except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 		#continuerospa
 		continue
+		
+def write_yaml_to_file(py_obj):
+    with open('output.yaml', 'w',) as f :
+        yaml.dump(py_obj,f,sort_keys=False) 
+    print('Written to file successfully')
+    
+    
+
